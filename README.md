49b4925d2c9682f552c9cd93be529810fbab3d86
Sarah Betack <msbetack@gmail.com>
2017-06-09 16:46:45 -0500

    add directions

--- README.md
+++ README.md
@@ -9,3 +9,12 @@
 | >=            | greater than or equal to   |
 | <             | less than                  |
 | >             | greater than               |
+| &&            | and                        |
+| ||            | or                         |
+
+
+### Your mission:
+* Make the table above in `index.html`
+* Open `index.html` in the browser
+* Open the developer tools chrome console (see previous lesson if you forget how to get there)
+* Follow the rest of the directions in `script.js`
---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+// We can compare numbers
+//Answer each question, then console log the code version to see if you get true or false.
+console.log("Is 10 is less than 150?")
+// 
+console.log(10 < 150)
+
+// puts "1 is greater than or equal to 0:"
+// puts 1 >= 0
+
+// # We can check whether data matches
+// # or does not match (works on any type)
+
+// puts "An apple is equal to an apple:"
+// p "apple" == "apple"
+
+// puts "An apple is equal to an orange:"
+// p "apple" == "orange"
+
+// puts "An apple is not equal to an apple:"
+// p "apple" != "apple"
+
+// puts "An apple is not equal to an orange:"
+// p "apple" != "orange"
+
+// # We can see whether two booleans are both true:
+// i_like_computers = true
+// i_like_sports = true
+// i_like_knitting = false
+// i_like_dogs = false
+// i_like_cats = true
+
+// puts "I like computers and I like sports:"
+// p i_like_computers && i_like_sports
+
+// puts "I like sports and I like knitting:"
+// p i_like_sports && i_like_knitting
+
+// # We can see whether one of two booleans is true:
+
+// puts "I like sports or I like knitting:"
+// p i_like_sports || i_like_knitting
+
+// puts "I like knitting or I like dogs:"
+// p i_like_knitting || i_like_dogs
+});
